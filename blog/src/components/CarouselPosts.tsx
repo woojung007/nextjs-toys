f7f1e397f485e9056ac851737667821a55eb7028
woojung007 <eeeeve007@gmail.com>
2024-11-06 17:00:25 +0900

    💄 9.16 필터가능한 포스트 페이지💡- 스타일링

--- blog/src/components/CarouselPosts.tsx
+++ blog/src/components/CarouselPosts.tsx
@@ -5,7 +5,7 @@ import { getNonFeaturedPosts } from "@service/posts";
 export default async function CarouselPosts() {
   const posts = await getNonFeaturedPosts();
   return (
-    <section className="my-4">
+    <section className="m-4">
       <h2 className="text-2xl font-bold my-2">You May Like</h2>
 
       <MultiCarousel>
