4d8f44710df90dbf927cc491aaf3464d2a78a921
woojung007 <eeeeve007@gmail.com>
2024-11-08 20:46:19 +0900

    ✨ 10.2 마크다운 뷰어💡- 라이브러리

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import MarkdownViewer from "@components/MarkdownViewer";
 import { getPostData } from "@service/posts";
 
 type Props = {
@@ -7,14 +8,12 @@ type Props = {
 };
 
 export default async function PostPage({ params: { slug } }: Props) {
-  // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어온다.
   const post = await getPostData(slug);
 
-  // 2. 데이터를 마크다운뷰어에 표기한다.
   return (
     <>
       <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
+      <MarkdownViewer content={post.content} />
     </>
   );
 }
--- blog/src/components/FeaturedPosts.tsx
+++ blog/src/components/FeaturedPosts.tsx
@@ -2,9 +2,7 @@ import PostsGrid from "@components/PostsGrid";
 import { getFeaturedPosts } from "@service/posts";
 
 const FeaturedPosts = async () => {
-  // 1. 모든 포스트 데이터를 읽어와야 함
   const posts = await getFeaturedPosts();
-  //  2. 모든 포스트 데이터를 보여줌
 
   return (
     <section className="m-4">
